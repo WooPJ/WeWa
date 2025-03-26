f884e0adc71f215518e591f98ba5e128f1629b91
kdm20204 <kdm20204@gmail.com>
2025-03-26 12:19:21 +0900

    s

--- src/main/java/com/springboot/wearwave/WearwaveApplication.java
+++ src/main/java/com/springboot/wearwave/WearwaveApplication.java
@@ -2,9 +2,17 @@ package com.springboot.wearwave;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WearwaveApplication {
+public class WearwaveApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+
+		return builder.sources(WearwaveApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WearwaveApplication.class, args);
